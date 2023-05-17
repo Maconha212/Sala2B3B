cba1b8c296e125dff3691d194790e600d63f4057
Maconha212 <126517319+Maconha212@users.noreply.github.com>
2023-05-17 10:31:23 -0400

    attk

--- JS/calculos.js
+++ JS/calculos.js
@@ -1,7 +1,20 @@
-function somarValores(){
-    var n1 = parseInt(numero1.value);
-    var n2 = parseInt(numero2.value);
-    var soma = n1 + n2;
+document.querySelector("#btnSomar").addEventListener("click",somarIMC);
 
-    resultado.value = soma;
+function somarIMC(){
+    var peso = parseFloat(document.querySelector("#peso").value);
+    var altura = parseFloat(document.querySelector("#altura").value);
+    var soma = peso / (altura * altura);
+
+    document.querySelector("#resultado").value = soma;
 }
+// const bSomar = document.querySelector("#btnSomar");
+// bSomar.addEventListener("click", ()=>{
+//     var n1 = parseFloat(document.querySelector("#numero1").value);
+//     var n2 = parseFloat(document.querySelector("#numero2").value);
+// })
+
+// function somar(p1, p2){
+//     var soma = p1+p2;
+//     document.querySelector("#resultado").value = soma;
+// }
+
